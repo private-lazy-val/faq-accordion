0c6d5a85616a9ab82fc7b3462815886ee61a94c2
Valeriya Evstratova <valelondonenko@yahoo.com>
2023-06-14 19:21:48 +0400

    feature: add accordion

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
 import './style.css';
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
-//
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-//
-// setupCounter(document.querySelector('#counter'))
+
+const faqAccordion = document.querySelector('.faq__accordion');
+const accordionItems = faqAccordion.querySelectorAll('.accordion__item');
+faqAccordion.addEventListener(('click'), (e) => {
+    const accordionItem = e.target.closest('.accordion__item');
+
+    if (accordionItem.classList.contains('active')) {
+        accordionItem.classList.remove('active');
+        accordionItem.querySelector('.accordion__answer').style.maxHeight = null;
+    } else {
+        accordionItems.forEach((item) => {
+            item.classList.remove("active");
+            item.querySelector('.accordion__answer').style.maxHeight = null;
+        });
+
+        accordionItem.classList.add('active');
+        let maxHeight = accordionItem.querySelector('.accordion__answer').scrollHeight + "px";
+        accordionItem.querySelector('.accordion__answer').style.maxHeight = maxHeight;
+    }
+});
