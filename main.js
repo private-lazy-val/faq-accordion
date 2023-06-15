e11e57e2c8a3248cf068ab2d5a9c602a1e49e82a
Valeriya Evstratova <valelondonenko@yahoo.com>
2023-06-15 19:36:40 +0400

    fix bugs

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
 import './style.css';
 
-const faqAccordion = document.querySelector('.faq__accordion');
-const accordionItems = faqAccordion.querySelectorAll('.accordion__item');
-faqAccordion.addEventListener(('click'), (e) => {
-    const accordionItem = e.target.closest('.accordion__item');
+const faqElement = document.querySelector('.faq__accordion');
+faqElement.addEventListener('click', (e) => {
+    if (e.target.classList.contains('accordion__question')) {
+        const accordionItem = e.target.closest('.accordion__item');
 
-    if (accordionItem.classList.contains('active')) {
-        accordionItem.classList.remove('active');
-        accordionItem.querySelector('.accordion__answer').style.maxHeight = null;
-    } else {
-        accordionItems.forEach((item) => {
-            item.classList.remove("active");
-            item.querySelector('.accordion__answer').style.maxHeight = null;
-        });
-
-        accordionItem.classList.add('active');
-        let maxHeight = accordionItem.querySelector('.accordion__answer').scrollHeight + "px";
-        accordionItem.querySelector('.accordion__answer').style.maxHeight = maxHeight;
+        if (accordionItem.open) {
+            accordionItem.open = false;
+        } else {
+            const accordionItems = document.querySelectorAll('.accordion__item');
+            accordionItems.forEach(item => item.open = false);
+            accordionItem.open = true;
+        }
+        e.preventDefault(); // prevent the default click event action
     }
 });
